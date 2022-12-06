753b8c6308f1353ab41b66990e46e2bc98c49f7b
bhavin-02 <bhaving1112@gmail.com>
2022-12-06 19:14:39 +0530

    First push

--- index.js
+++ index.js
@@ -3,15 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/app';
+import App from './src/App';
 import {name as appName} from './app.json';
-import { Amplify } from "aws-amplify";
-import awsExports from "./src/aws-exports";
 
-
-if(!__DEV__){
-    console.log = () => {};
-}
-
-Amplify.configure(awsExports);
 AppRegistry.registerComponent(appName, () => App);
